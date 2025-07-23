f812d1faf9230cf24cf11034948861d048e4f2a0
WWWWYYYYAAAA <767210874@qq.com>
2025-07-23 21:16:24 +0800

    FC0

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -31,9 +31,12 @@
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
+from .base.legged_robot_StageR import LeggedRobotSR
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from legged_gym.envs.go1.go1_config import Go1RoughCfg, Go1RoughCfgPPO
 from legged_gym.envs.aliengo.aliengo_config import AlienGoRoughCfg, AlienGoRoughCfgPPO
+from legged_gym.envs.zsl1.zsl1_config import ZSL1RoughCfg, ZSL1RoughCfgPPO
+from legged_gym.envs.zsl1.zsl1_plain_config import ZSL1PlainCfg, ZSL1PlainCfgPPO
 
 import os
 
@@ -42,3 +45,6 @@ from legged_gym.utils.task_registry import task_registry
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "go1", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO() )
 task_registry.register( "aliengo", LeggedRobot, AlienGoRoughCfg(), AlienGoRoughCfgPPO() )
+task_registry.register( "zsl1", LeggedRobot, ZSL1RoughCfg(), ZSL1RoughCfgPPO() )
+task_registry.register( "zsl1", LeggedRobotSR, ZSL1PlainCfg(), ZSL1PlainCfgPPO() )
+
